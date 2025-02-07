04aaffd9333ac88b9210a63c3a17f9e247f88453
Rishi <rishivishal96@gmail.com>
2025-02-07 10:54:09 -0500

    initial commits

---
+++ Desktop/python Funtions/Input-1.py
@@ -0,0 +1,14 @@
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+
+Age = input("How old are you? ")
+print("your age is " + Age)
+
+background = input("What is your background? ")
+print("your background is " + background)
+
+college = input("What is your college name? ")
+print("your college name is " + college)
+
+student_ID = input("What is your student ID? ")
+print("your student ID is " + student_ID)
